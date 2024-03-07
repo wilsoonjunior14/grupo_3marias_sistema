4c0a95f2df7f38233430ea8ad4d2b8074869ede5
wilson <wilsonjuniorfoda>
2024-03-07 16:47:38 -0300

    adding tests for contract creation

--- 3marias/app/Models/CategoryProduct.php
+++ 3marias/app/Models/CategoryProduct.php
@@ -10,14 +10,16 @@ class CategoryProduct extends BaseModel
     static $fieldsToBeUpdated = ["name", "category_products_father_id"];
 
     static $rules = [
-        'name' => 'bail|required|string|max:255|min:3'
+        'name' => 'bail|required|string|max:255|min:3',
+        'category_products_father_id' => 'integer'
     ];
 
     static $rulesMessages = [
         'name.required' => 'Campo Nome da Categoria do Produto é obrigatório.',
         'name.max' => 'Campo Nome da Categoria do Produto permite no máximo 255 caracteres.',
         'name.min' => 'Campo Nome da Categoria do Produto deve conter no mínimo 3 caracteres.',
-        'name.string' => 'Campo Nome da Categoria do Produto está inválido.'
+        'name.string' => 'Campo Nome da Categoria do Produto está inválido.',
+        'category_products_father_id.integer' => 'Campo Identificador da Categoria de Produto está inválido.'
     ];
 
     public function category_product(): \Illuminate\Database\Eloquent\Relations\HasOne {
