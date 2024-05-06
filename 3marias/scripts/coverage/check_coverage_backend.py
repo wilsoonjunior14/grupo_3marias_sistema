ecb4521b0a1b1631f0548a5a246a4f415b6cd765
wilson <wilsonjuniorfoda>
2024-05-06 17:02:43 -0300

    update check_code_coverage

--- 3marias/scripts/coverage/check_coverage_backend.py
+++ 3marias/scripts/coverage/check_coverage_backend.py
@@ -20,16 +20,7 @@ with open('../coverage_reports/dashboard.html', 'r') as file:
             else:
                 value = int(item.text.split("%")[0])
 
-                if (not className.startswith("App\\Http\\Middleware\\") 
-                    and not className.startswith("App\\Providers")
-                    and not className.startswith("App\\Console")
-                    and not className.startswith("App\\Mail")
-                    and not className.startswith("App\\Http\\Controllers\\")
-                    and not className.startswith("App\\Models\\")
-                    and not className.startswith("App\\Exceptions")
-                    and not className.endswith("BotManController")
-                    and not className.endswith("FileBusiness")):
-                    
+                if (className.startswith("App\\Business\\")): 
                     if (value < 75):
                         raise Exception("\nThe file "+className+" has "+str(value)+"% of code coverage, but it is expected 75% at least\n")
             index = index + 1
