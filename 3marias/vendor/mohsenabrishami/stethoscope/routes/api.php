1f32359cd8983437a19d96f2d7fcc3ef62d6139e
wilson <wilsonjuniorfoda>
2024-05-15 00:51:15 -0300

    removing vendor folder

--- 3marias/vendor/mohsenabrishami/stethoscope/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use MohsenAbrishami\Stethoscope\Http\Controllers\MonitorController;
-
-Route::middleware(['check.access.to.monitoring.panel'])->group(function () {
-    Route::get('monitor/current', [MonitorController::class, 'current']);
-    Route::get('monitor/history/{from}/{to}', [MonitorController::class, 'history']);
-});
