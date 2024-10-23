4b123d7b11ca1c2f8714b1a9f2443b8c0d88b896
wilson <wilsonjuniorfoda>
2024-10-23 16:02:34 -0300

    removing url blockers

--- 3marias/app/Http/Middleware/UserIsAllowed.php
+++ 3marias/app/Http/Middleware/UserIsAllowed.php
@@ -18,40 +18,40 @@ class UserIsAllowed
      */
     public function handle(Request $request, Closure $next)
     {
-        $currentUser = $request->user();
-        $roleObj = new Role();
-        $groupRoleInstance = new GroupRole();
+        // $currentUser = $request->user();
+        // $roleObj = new Role();
+        // $groupRoleInstance = new GroupRole();
 
-        $pattern = "/(\d+)/i";
-        $patternReplaced = "{id}";
+        // $pattern = "/(\d+)/i";
+        // $patternReplaced = "{id}";
 
-        $dataRequest = [
-            'endpoint' => preg_replace($pattern, $patternReplaced, str_replace("/api/v1", "", $_SERVER['REQUEST_URI'])),
-            'request_method' => strtolower($_SERVER['REQUEST_METHOD'])
-        ];
+        // $dataRequest = [
+        //     'endpoint' => preg_replace($pattern, $patternReplaced, str_replace("/api/v1", "", $_SERVER['REQUEST_URI'])),
+        //     'request_method' => strtolower($_SERVER['REQUEST_METHOD'])
+        // ];
 
-        $roles = $roleObj->getRolesByRequestTypeAndEndpoint($dataRequest["request_method"], $dataRequest["endpoint"]);
+        // $roles = $roleObj->getRolesByRequestTypeAndEndpoint($dataRequest["request_method"], $dataRequest["endpoint"]);
 
-        if (count($roles) == 0){
-            return response()->json(
-                [
-                    'errors' => [
-                        'request' => 'Operação desconhecida não pode ser realizada.'
-                    ]
-                ]
-            , 404);
-        }
+        // if (count($roles) == 0){
+        //     return response()->json(
+        //         [
+        //             'errors' => [
+        //                 'request' => 'Operação desconhecida não pode ser realizada.'
+        //             ]
+        //         ]
+        //     , 404);
+        // }
 
-        $groupRoles = $groupRoleInstance->getGroupRolesByGroupForRequest(
-            $currentUser->group_id, $dataRequest["request_method"], $dataRequest["endpoint"]);
+        // $groupRoles = $groupRoleInstance->getGroupRolesByGroupForRequest(
+        //     $currentUser->group_id, $dataRequest["request_method"], $dataRequest["endpoint"]);
 
-        if (count($groupRoles) === 0) {
-            return response()->json(
-                [
-                    "errors" => ["request" => "Você não tem permissão para realizar essa operação."]
-                ]
-            , 401);
-        }
+        // if (count($groupRoles) === 0) {
+        //     return response()->json(
+        //         [
+        //             "errors" => ["request" => "Você não tem permissão para realizar essa operação."]
+        //         ]
+        //     , 401);
+        // }
 
         return $next($request);
     }
